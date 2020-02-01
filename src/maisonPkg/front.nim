0ded1fc525d6d925770b76a1f0229f08bcd9d1b0
Christine Dodrill <me@christine.website>
2020-02-01 18:41:24 +0000

    don't cache front requests

--- src/maisonPkg/front.nim
+++ src/maisonPkg/front.nim
@@ -4,16 +4,7 @@ type
   Front* = object
     who*: string
 
-proc front(hc = newAsyncHttpClient()): Future[string] {.async.} =
+proc frontCached*(hc = newAsyncHttpClient()): Future[string] {.async.} =
   var front = await hc.getContent(getEnv "FRONT_URL")
   front.stripLineEnd
   return front
-
-proc frontCached*(hc = newAsyncHttpClient()): Future[Front] {.async.} =
-  let url = getEnv "FRONT_URL"
-  try:
-    result = await loadFromCache[Front](url)
-  except NotCachedException:
-    result = Front(who: await front(hc))
-    await cacheData[Front](url, result)
-    info "grabbed front from remote host"
